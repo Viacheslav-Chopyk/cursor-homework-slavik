9dde04509d440504474712b14f05ab46f88acbe2
chopyk89 <36411310+chopyk89@users.noreply.github.com>
2021-06-03 13:28:29 -0500

    Update src.js

---
+++ 02-test-homework/src.js
@@ -0,0 +1,31 @@
+let n, m;
+let sum = 0;
+
+function inputN() {
+    do {
+        n = +prompt("Enter first integer number");
+    } while ((Object.is(NaN, n) === true) || (n !== parseInt(n)))
+}
+function inputM() {
+    do {
+        m = +prompt("Enter last number integer");
+    } while ((Object.is(NaN, m) === true) || (m !== parseInt(m)))
+}
+
+inputN();
+inputM();
+
+
+let addEvenNumber = confirm('Add Even numbers?');
+
+for (n; n <= m; n++) {
+    if (addEvenNumber) {
+        if (n % 2 === 0) {
+            continue;
+        }
+        sum += n;
+    } else {
+        sum += n;
+    }
+}
+console.log(sum);
