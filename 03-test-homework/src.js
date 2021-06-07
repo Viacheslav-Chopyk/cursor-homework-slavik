9c82e89f1e01d81b9212b6fcd3beea665b6e6445
Chopyk <if040489cvv@gmail.com>
2021-06-07 14:39:27 +0300

    updates

--- 03-test-homework/src.js
+++ 03-test-homework/src.js
@@ -8,4 +8,15 @@ function getMaxDigit(number) {
         return Math.max(number % 10, getMaxDigit(number / 10))
     }
 }
-console.log(getMaxDigit(472658));
+//console.log(getMaxDigit(472658));
+
+// Task 2 -----------------
+
+function getPowOfNumber(x, n) {
+    let result = 1;
+    for (let i = 0; i < n; i++) {
+        result = result * x;
+    }
+    return result;
+}
+console.log(getPowOfNumber(2, 2));
