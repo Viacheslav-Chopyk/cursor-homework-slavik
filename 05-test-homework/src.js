141442376840e4af1b0a8cd092ae1f03d393ac82
Slavik Chopyk <if040489cvv@gmail.com>
2021-06-19 23:22:37 +0200

    new

---
+++ 05-test-homework/src.js
@@ -0,0 +1,69 @@
+// Task 1 Створіть функцію getRandomArray(length, min, max) – яка повертає масив випадкових цілих чисел.
+// У функції є параметри: length - довжина масиву, min – мінімальне значення цілого числа, max – максимальне значення цілого числа. 
+// Приклад: getRandomArray(15, 1, 100) –> [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2]
+
+function getRandomArray(length, min, max) {
+    const arr1 = [];
+    for (let i = 0; i < length; i++) {
+        arr1[i] = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    return arr1;
+}
+console.log(getRandomArray(15, 1, 100));
+
+// Task 2 Створіть функцію getModa(...numbers) – яка вираховує моду всіх переданих в неї аргументів. НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
+// Приклад: getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –> 2
+
+// Task 3 Створіть функцію getAverage(...numbers) – яка рахує середнє арифметичне всіх переданих в неї аргументів. НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
+// Приклад: getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –> 34.4
+
+function getAverage(...numbers) {
+    let arr3 = numbers.filter(num => Number.isInteger(num));
+    return arr3.reduce((prev, current) => {
+        return (prev + current);
+    }, 0) / arr3.length;
+
+}
+console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+
+// Task 4 Створіть функцію getMedian(...numbers) – яка рахує медіану всіх переданих в неї аргументів. НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
+// Приклад: getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –> 23 Приклад: getMedian(1, 2, 3, 4) –> 2.5 Приклад: getMedian(1, 2, 3, 4, 5) –> 3
+
+function getMedian(...numbers) {
+    let median = 0;
+    const arr4 = numbers.filter(num => Number.isInteger(num)).sort((a, b) => a - b);
+
+    if (arr4.length % 2 === 0) {
+        median = (arr4[arr4.length / 2 - 1] + arr4[arr4.length / 2]) / 2;
+    } else {
+        median = arr4[Math.floor(arr4.length / 2)];
+    }
+    return median;
+}
+console.log(getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+
+// Task 5 Створіть функцію filterEvenNumbers(...numbers) – яка фільтрує парні числа передані як аргументи функції
+// Приклад: filterEvenNumbers(1, 2, 3, 4, 5, 6) -> [1, 3, 5]
+
+function filterEvenNumbers(...numbers) {
+    return numbers.filter(number => number % 2 !== 0);
+
+}
+console.log(filterEvenNumbers(1, 2, 3, 4, 5, 6));
+
+// Task 6 Створіть функцію countPositiveNumbers(...numbers) – яка порахує кількість чисел більших 0
+// Приклад: countPositiveNumbers(1, -2, 3, -4, -5, 6) -> 3
+
+function countPositiveNumbers(...numbers) {
+    return numbers.filter(numb => numb > 0).length;
+
+}
+console.log(countPositiveNumbers(1, -2, 3, -4, -5, 6));
+
+// Task 7 Створіть функцію getDividedByFive(...numbers) – яка відфільтрує усі елементи в масиві та залишить тільки ті, які діляться на ціло на 5
+// Приклад: getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) -> [55, 55]
+
+function getDividedByFive(...numbers) {
+    return numbers.filter(number => number % 5 === 0);
+}
+console.log(getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
