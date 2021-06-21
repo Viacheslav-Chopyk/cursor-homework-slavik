152d27ca7cd3c83aca94249529c92ade837c5f3c
chopyk89 <36411310+chopyk89@users.noreply.github.com>
2021-06-21 10:19:01 +0200

    Update src.js

--- 05-test-homework/src.js
+++ 05-test-homework/src.js
@@ -67,3 +67,13 @@ function getDividedByFive(...numbers) {
     return numbers.filter(number => number % 5 === 0);
 }
 console.log(getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+
+// Task 8.Створіть функцію divideByThree(word), яка розбиває кожне слово на умовні склади по 3 букви. Якщо букв менше трьох – не розбиває. Пробіли завжди видаляються. Рядок приводится до нижнього регістру. 
+// Приклад: divideByThree("Commander) -> ["com", "man", "der"] Приклад: divideByThree("live") -> ["liv", "e"]
+
+function divideByThree(str) { 
+  str = str.replace(/\s/g, '').toLowerCase() 
+  return str.match(/.{1,3}/g); 
+}
+
+console.log(divideByThree('Commander'));
