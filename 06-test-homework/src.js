1d0e975439cdca8cc6cacedbb0e3f22276b805c6
Slavik Chopyk <if040489cvv@gmail.com>
2021-06-21 13:16:57 +0200

    new

--- 06-test-homework/src.js
+++ 06-test-homework/src.js
@@ -43,6 +43,44 @@ console.log(getSubjects(students[0]));
 // 2. Створіть функцію getAverageMark(students[0]) --> 3.79 – яка поверне середню оцінку по усім предметам для переданого студента НЕ МАСИВА СТУДЕНТІВ. 
 // Оцінку округліть до 2ого знаку. Можна використовувати функції, написані у попередніх домашніх завданнях :)
 
-function getAverageMark() {
+function getAverageMark(students) {
+    const studentSubjects = students.subjects;
+    const allMarks = [];
+
+    for (subject in studentSubjects) allMarks.push(...studentSubjects[subject]);
+
+    let marksSum = allMarks.reduce((total, prev) => total + prev, 0);
+    let marksCount = allMarks.length;
+
+    return +(marksSum / marksCount).toFixed(2);
+}
+console.log(getAverageMark(students[0]));
+
+// Task 3. Створіть функцію getStudentInfo(students[0]) --> { "course": 3, "name": "Tanya", "averageMark": 3.79} – яка повертає інформацію загального виду по переданому студенту (вам знадобиться функція з попереднього завдання). 
+// Пoвинна бути виведена інформація: курс, ім'я, середня оцінка.
+
+function getStudentInfo(students) {
+    const { course, name } = students;
+    const averageMark = getAverageMark(students);
+    return { course, name, averageMark };
+
+}
+console.log(getStudentInfo(students[0]));
+
+// Task 4. Ствроіть функцію getStudentsNames(students) --> ["Anton", "Tanya, "Victor"] – яка повертає імена студентів у алфавітному порядку.
+
+const getStudentsNames = students => students.map(student => student.name).sort();
+console.log(getStudentsNames(students));
+
+// Task 5. Створіть функцію getBestStudent(students) --> "Anton" – яка повертає кращого студента зі списку по показнику середньої оцінки.
+
+function getBestStudent(students) {
+
+}
+console.log(getBestStudent(students));
+
+// Task 6. Створіть функцію calculateWordLetters("тест") --> { "т": 2, "е": 1, "с": 1 } – яка повертає обє'кт, в якому ключі це букви у слові, а значення – кількість їх повторень.
+
+function calculateWordLetters(str) {
 
 }
