e84504bfe9cdfba0e71654908d321ddd1115f15e
Slavik Chopyk <if040489cvv@gmail.com>
2021-06-21 11:34:27 +0200

    new

--- 06-test-homework/src.js
+++ 06-test-homework/src.js
@@ -24,3 +24,25 @@ const students = [{
     }
 }];
 
+// 1. Task Створіть функцію getSubjects(students[0] --> ["Math", "Algorithms", "Data science"] - яка повертає список предметів для конкретного студента. 
+/// Зверніть увагу – назву предмету необхідно повертати з великої літери, а _ – замінити на пробіл
+
+function getSubjects(students) {
+    let listSubjects = Object.keys(students.subjects);
+
+    listSubjects = listSubjects.map((item) => {
+        return item[0].toUpperCase() + item.slice(1).replaceAll("_", "-");
+    });
+
+    return listSubjects;
+}
+
+console.log(getSubjects(students[0]));
+
+
+// 2. Створіть функцію getAverageMark(students[0]) --> 3.79 – яка поверне середню оцінку по усім предметам для переданого студента НЕ МАСИВА СТУДЕНТІВ. 
+// Оцінку округліть до 2ого знаку. Можна використовувати функції, написані у попередніх домашніх завданнях :)
+
+function getAverageMark() {
+
+}
