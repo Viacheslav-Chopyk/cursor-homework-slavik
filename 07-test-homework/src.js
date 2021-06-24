3c5fef9fc23e8a0df199af161a7f43c3736ef83d
Chopyk <if040489cvv@gmail.com>
2021-06-24 12:55:27 +0300

    new

---
+++ 07-test-homework/src.js
@@ -0,0 +1,48 @@
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+
+
+// Task 1. Створіть функцію getMyTaxes.call(country, salary) -> number; – яка рахує скільки податків ви заплатите як IT-спеціаліст в якійсь з країн.
+// Функція повинна викликатись через call та працювати з даними через this
+
+function getMyTaxes(salary){
+    return this.tax*salary;
+}
+console.log(getMyTaxes.call(ukraine, 1200));
+
+// Task 2. Створіть функцію getMiddleTaxes.call(country) -> number; – яка рахує скільки у середньому податків платятт IT-спеціалісти у кожній країні. (tax * middleSalary).
+// Функція повинна викликатись через call та працювати з даними через this
+
+function getMiddleTaxes() {
+    return this.tax*this.middleSalary;
+}
+console.log(getMiddleTaxes.call(latvia));
+
+// Task 3. Створіть функцію getTotalTaxes.call(country) -> number; – яка рахує, скільки всього податків платять IT-спеціалісти у кожній країні. (tax * middleSalary * vacancies).
+// Функція повинна викликатись через call та працювати з даними через this
+
+function getTotalTaxes(){
+    return this.tax*this.middleSalary*this.vacancies;
+}
+console.log(getTotalTaxes.call(ukraine));
+// Task 4. Створіть функцію getMySalary(country) – яка буде писати в консоль об'єкт виду: { salary: number, taxes: number, profit: number } кожні 10 секунд.
+// Значення salary – генеруйте випадковим чином у діапазоні 1500-2000. taxes – розраховується в залежності від вибраної країни та значення salary.
+// profit = salary - taxes;
+
+function getMySalary(){
+    setInterval(()=>
+        { const salary = 1500 + Math.random() * (2000 - 1500);
+            const taxes = this.tax * salary;
+            const profit = salary - taxes;
+            const res = {
+                salary: salary,
+                taxes: taxes,
+                profit: profit,
+            };
+            console.log(res);
+        }
+        ,10000)
+}
+getMySalary.call(ukraine);
